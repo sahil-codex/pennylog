61b1e4b383ed13f085076d43f3609cbf92c047df
Sahil-codex <sahil2flamingo@gmail.com>
2026-03-18 22:35:58 +0530

    db.ts error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,6 @@
 import {neon} from "@neondatabase/serverless";
 
+if(!process.env.DATABASE_URL){
+      throw new Error("DATABASE_URL is not defined");
+}
 export const sql = neon(process.env.DATABASE_URL!);
