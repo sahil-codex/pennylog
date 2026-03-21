93bfa7f82e6903f2e8da9ec2ecc38679be903bdb
Sahil-codex <sahil2flamingo@gmail.com>
2026-03-21 19:33:55 +0530

    debugg11

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,10 +10,9 @@ export default function DashboardLayout({
                 <aside className="w-64 bg-white shadow-md p-6 flex flex-col justify-between">
                     <div>
                         <h1 className="text-2xl font-bold mb-8">PennyLog</h1>
-                        <nav className="flex flex-col gap-4">
-                            <Link href="/dashboard" className="text-gray-700 hover:text-black">
-                              📊  Dashboard</Link>
-                              <Link href="/dashboard/transactions" className="text-gray-700 hover:text-black">
+                        <nav className="flex flex-col gap-2">
+                            <Link href="/dashboard" className={`flex items-center gap-2 p-2 rounded-lg ${pathname === "/dashboard" ? "bg-gray-200 text-black font-semibold" : "text-gray-700 hover:bg-gray-100"}`}>📊 Dashboard </Link>
+                              <Link href="/dashboard/transactions" className={`flex items-center gap-2 p-2 rounded-lg ${ pathname ==="/dashboard/transactions"?"bg-gray-200 text-black font-semibold":"text-gray-700 hover:bg-gray-100"}`}>
                                  💸 Transactions</Link>
                         </nav>
                     </div>
