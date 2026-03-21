2911101235856174c4ada1e5e973698f3b81d51f
Sahil-codex <sahil2flamingo@gmail.com>
2026-03-21 22:48:07 +0530

    add transaction

---
+++ src/app/dashboard/transactions/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Transaction(){
+    return (
+        <div>
+        <h1 className="text-2xl font-bold mb-4">Transactions</h1>
+        <p>All your transactions will appear here.</p>
+      </div>
+     );
+    }
