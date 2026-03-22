d0b8bcb11da1f794c1fbaea64901a4a6e4031015
Sahil-codex <sahil2flamingo@gmail.com>
2026-03-22 10:17:48 +0530

    logout errorfix

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 
-export async function GET(){
+export async function POST(){
     const response = NextResponse.json({
         message: "Logged out successfully",
 
